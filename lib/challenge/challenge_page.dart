39cc0b40d776d3c51c0c4d48a7fbda0e4c1d968e
Rafael Chianca <rafachianca@gmail.com>
2022-03-03 22:23:28 -0300

    💄 Adds UI for challenge page and its components

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,34 @@
+import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  const ChallengePage({Key? key}) : super(key: key);
+
+  @override
+  State<ChallengePage> createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: QuestionIndicatorWidget(),
+        )),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            QuizWidget(title: 'O que o Flutter faz em sua totalidade?')
+          ],
+        ),
+      ),
+    );
+  }
+}
