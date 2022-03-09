dd4f2994800035472d73706be3e4fdbdbab2d06d
Rafael Chianca <rafachianca@gmail.com>
2022-03-09 01:50:52 -0300

    ✨ Adds answer prop to widget

--- lib/challenge/widgets/answer/answer_wisget.dart
+++ lib/challenge/widgets/answer/answer_wisget.dart
@@ -1,79 +1,86 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
 import 'package:flutter/material.dart';
 
 class AnswerWidget extends StatelessWidget {
-  final String title;
-  final bool isCorrect;
+  final AnswerModel answer;
   final bool isSelected;
+  final VoidCallback onTap;
 
-  const AnswerWidget(
-      {Key? key,
-      required this.title,
-      this.isCorrect = false,
-      this.isSelected = false})
-      : super(key: key);
+  const AnswerWidget({
+    Key? key,
+    required this.answer,
+    this.isSelected = false,
+    required this.onTap,
+  }) : super(key: key);
 
   Color get _selectedColorCorrect =>
-      isCorrect ? AppColors.darkGreen : AppColors.darkRed;
+      answer.isCorrect ? AppColors.darkGreen : AppColors.darkRed;
 
   Color get _selectedBorderCorrect =>
-      isCorrect ? AppColors.lightGreen : AppColors.lightRed;
+      answer.isCorrect ? AppColors.lightGreen : AppColors.lightRed;
 
   Color get _selectedColorCardCorrect =>
-      isCorrect ? AppColors.lightGreen : AppColors.lightRed;
+      answer.isCorrect ? AppColors.lightGreen : AppColors.lightRed;
 
   Color get _selectedBorderCardCorrect =>
-      isCorrect ? AppColors.green : AppColors.red;
+      answer.isCorrect ? AppColors.green : AppColors.red;
 
-  TextStyle get _selectedTextStyleCorrect =>
-      isCorrect ? AppTextStyles.bodyDarkGreen : AppTextStyles.bodyDarkRed;
+  TextStyle get _selectedTextStyleCorrect => answer.isCorrect
+      ? AppTextStyles.bodyDarkGreen
+      : AppTextStyles.bodyDarkRed;
 
-  IconData get _selectedIconCorrect => isCorrect ? Icons.check : Icons.close;
+  IconData get _selectedIconCorrect =>
+      answer.isCorrect ? Icons.check : Icons.close;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8),
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: isSelected ? _selectedColorCardCorrect : AppColors.white,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.fromBorderSide(BorderSide(
-                color: isSelected
-                    ? _selectedBorderCardCorrect
-                    : AppColors.border))),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Text(
-                title,
-                style:
-                    isSelected ? _selectedTextStyleCorrect : AppTextStyles.body,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: isSelected ? _selectedColorCardCorrect : AppColors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.fromBorderSide(BorderSide(
+                  color: isSelected
+                      ? _selectedBorderCardCorrect
+                      : AppColors.border))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(
+                  answer.title,
+                  style: isSelected
+                      ? _selectedTextStyleCorrect
+                      : AppTextStyles.body,
+                ),
               ),
-            ),
-            SizedBox(width: 30),
-            Container(
-              height: 24,
-              width: 24,
-              decoration: BoxDecoration(
-                  color: isSelected ? _selectedColorCorrect : AppColors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.fromBorderSide(BorderSide(
-                      color: isSelected
-                          ? _selectedBorderCorrect
-                          : AppColors.border))),
-              child: isSelected
-                  ? Icon(
-                      _selectedIconCorrect,
-                      size: 16,
-                      color: Colors.white,
-                    )
-                  : null,
-            )
-          ],
+              SizedBox(width: 30),
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                    color: isSelected ? _selectedColorCorrect : AppColors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.fromBorderSide(BorderSide(
+                        color: isSelected
+                            ? _selectedBorderCorrect
+                            : AppColors.border))),
+                child: isSelected
+                    ? Icon(
+                        _selectedIconCorrect,
+                        size: 16,
+                        color: Colors.white,
+                      )
+                    : null,
+              )
+            ],
+          ),
         ),
       ),
     );
